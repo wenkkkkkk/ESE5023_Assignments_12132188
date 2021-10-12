dd3bd90ea672d9f9ea628920445d7116099fe2fc
wenkkkkkk <90738739+wenkkkkkk@users.noreply.github.com>
2021-10-12 17:06:34 +0800

    Add files via upload

---
+++ PS1_4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 16:44:44 2021
+
+@author: Administrator
+"""
+
+def Least_moves(k):
+    i = 0
+    while k >1:
+        if k%2 == 1:
+            k = k - 1
+            i = i + 1
+        else:
+            k = k / 2
+            i = i + 1
+    return i
+    print(i)
